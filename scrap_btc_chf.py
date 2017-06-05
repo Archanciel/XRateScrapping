429f0dc5636e5726b03a3912dee87f0ffda0b988
Archanciel <jp.schnyder@gmail.com>
2017-06-05 22:15:57 +0200

    refactoring

--- scrap_btc_chf.py
+++ scrap_btc_chf.py
@@ -8,16 +8,26 @@ def cleanRate(rateStr):
     '''
     rateStr = rateStr.replace('.','')
     return rateStr.replace(',','.')
-    
-url = "https://fr.investing.com/currencies/btc-chf"
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'}) 
-page = urlopen(req).read()
 
-soup = BeautifulSoup(page)
-#print(soup.prettify())
 
-rateStr = soup.find(id='last_last').string
-rateStr = cleanRate(rateStr)
-print(rateStr)
+def getBTCRate(targetCur):
+    '''
+    Scrap the bitcoin rate in the passed currency
+    :param targetCur: currency in which the bitcoin rate is fetched
+    :return: bitcoin rate in targetCur
+    '''
+    url = "https://fr.investing.com/currencies/btc-" + targetCur.lower()
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    page = urlopen(req).read()
+    soup = BeautifulSoup(page,"lxml")
+    # print(soup.prettify())
+    rateStr = soup.find(id='last_last').string
+    rateStr = cleanRate(rateStr)
+    return rateStr
+
+if __name__ == "__main__":
+    print("BTC/CHF: " + getBTCRate("CHF"))
+    print("BTC/EUR: " + getBTCRate("EUR"))
+    print("BTC/USD: " + getBTCRate("USD"))
 
 
