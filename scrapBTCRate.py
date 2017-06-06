25d3c4e36254cbfeb13c326d0eb6c4b15ebd120f
Archanciel <jp.schnyder@gmail.com>
2017-06-06 19:21:02 +0200

    changed site url

--- scrapBTCRate.py
+++ scrapBTCRate.py
@@ -6,22 +6,23 @@ def cleanRate(rateStr):
     remove thousands divider from passed 
     rate string and return cleaned string
     '''
-    rateStr = rateStr.replace('.','')
-    return rateStr.replace(',','.')
+    return rateStr.replace(',','')
 
 
 def getBTCRate(targetCur):
     '''
     Scrap the bitcoin rate in the passed currency
+    Usage: getBTCRate("CHF"), getBTCRate("USD")
     :param targetCur: currency in which the bitcoin rate is fetched
     :return: bitcoin rate in targetCur
     '''
-    url = "https://fr.investing.com/currencies/btc-" + targetCur.lower()
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    url = "http://markets.businessinsider.com/currencies/realtime-chart/btc-" + targetCur.lower()
+    #req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request(url)
     page = urlopen(req).read()
     soup = BeautifulSoup(page,"lxml")
     # print(soup.prettify())
-    rateStr = soup.find(id='last_last').string
+    rateStr = soup.find("span",{'class':'push-data price'}).string
     rateStr = cleanRate(rateStr)
     return rateStr
 
